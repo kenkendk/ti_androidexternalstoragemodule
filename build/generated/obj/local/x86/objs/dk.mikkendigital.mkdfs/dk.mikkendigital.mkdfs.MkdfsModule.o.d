a7f384495e04ea801c5d4b491490b9c934d61d00
Michiel Dondorp <Michiel@michiels-mbp.fritz.box>
2014-06-04 17:26:27 +0200

    Now got it working on 3.2.3.GA Changed project settings, copy pasted some from a default new project.

---
+++ build/generated/obj/local/x86/objs/dk.mikkendigital.mkdfs/dk.mikkendigital.mkdfs.MkdfsModule.o.d
@@ -0,0 +1,188 @@
+/var/folders/36/0y6y3zls7h70v6tz9d5fqmbw0000gn/T//Michiel/mkdfs-generated/obj/local/x86/objs/dk.mikkendigital.mkdfs/dk.mikkendigital.mkdfs.MkdfsModule.o: \
+ jni/dk.mikkendigital.mkdfs.MkdfsModule.cpp \
+ jni/dk.mikkendigital.mkdfs.MkdfsModule.h \
+ /Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/jni.h \
+ /Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/sys/cdefs.h \
+ /Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h \
+ /Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/android/api-level.h \
+ /Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stdarg.h \
+ /Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/_cprolog.h \
+ /Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/features.h \
+ /Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h \
+ /Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/user_config.h \
+ /Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/compat.h \
+ /Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/host.h \
+ /Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/_system.h \
+ /Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/_android.h \
+ /Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h \
+ /Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h \
+ /Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h \
+ /Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/_epilog.h \
+ /Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h \
+ /Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/Proxy.h \
+ /Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/v8.h \
+ /Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/v8stdint.h \
+ /Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stddef.h \
+ /Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stdio.h \
+ /Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/../include/stdio.h \
+ /Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/sys/types.h \
+ /Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/stdint.h \
+ /Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/sys/_types.h \
+ /Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/machine/_types.h \
+ /Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/linux/posix_types.h \
+ /Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/linux/stddef.h \
+ /Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/linux/compiler.h \
+ /Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/asm/posix_types.h \
+ /Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/asm/posix_types_32.h \
+ /Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/asm/types.h \
+ /Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/linux/types.h \
+ /Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/machine/kernel.h \
+ /Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/sys/sysmacros.h \
+ /Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/JavaObject.h \
+ /Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/assert.h \
+ /Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/../include/assert.h \
+ /Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/EventEmitter.h \
+ /Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/NativeObject.h \
+ /Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/AndroidUtil.h \
+ /Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/android/log.h \
+ /Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stdlib.h \
+ /Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/../include/stdlib.h \
+ /Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/string.h \
+ /Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/../include/string.h \
+ /Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/malloc.h \
+ /Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/alloca.h \
+ /Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/strings.h \
+ /Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/memory.h \
+ /Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/EventEmitter.h \
+ /Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/JNIUtil.h \
+ /Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/AndroidUtil.h \
+ /Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/JSException.h \
+ /Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/ProxyFactory.h \
+ /Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/TypeConverter.h \
+ /Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/V8Util.h \
+ /Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/org.appcelerator.kroll.KrollModule.h \
+ /Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/Proxy.h
+
+jni/dk.mikkendigital.mkdfs.MkdfsModule.h:
+
+/Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/jni.h:
+
+/Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/sys/cdefs.h:
+
+/Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/sys/cdefs_elf.h:
+
+/Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/android/api-level.h:
+
+/Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stdarg.h:
+
+/Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/_cprolog.h:
+
+/Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/features.h:
+
+/Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/_stlport_version.h:
+
+/Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/user_config.h:
+
+/Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/compat.h:
+
+/Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/host.h:
+
+/Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/_system.h:
+
+/Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/_android.h:
+
+/Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/_gcc.h:
+
+/Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/stl_confix.h:
+
+/Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/config/_native_headers.h:
+
+/Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/_epilog.h:
+
+/Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stl/_config_compat_post.h:
+
+/Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/Proxy.h:
+
+/Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/v8.h:
+
+/Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/v8stdint.h:
+
+/Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stddef.h:
+
+/Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stdio.h:
+
+/Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/../include/stdio.h:
+
+/Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/sys/types.h:
+
+/Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/stdint.h:
+
+/Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/sys/_types.h:
+
+/Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/machine/_types.h:
+
+/Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/linux/posix_types.h:
+
+/Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/linux/stddef.h:
+
+/Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/linux/compiler.h:
+
+/Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/asm/posix_types.h:
+
+/Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/asm/posix_types_32.h:
+
+/Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/asm/types.h:
+
+/Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/linux/types.h:
+
+/Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/machine/kernel.h:
+
+/Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/sys/sysmacros.h:
+
+/Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/JavaObject.h:
+
+/Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/assert.h:
+
+/Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/../include/assert.h:
+
+/Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/EventEmitter.h:
+
+/Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/NativeObject.h:
+
+/Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/AndroidUtil.h:
+
+/Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/android/log.h:
+
+/Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/stdlib.h:
+
+/Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/../include/stdlib.h:
+
+/Users/Michiel/android-ndk-r9d/sources/cxx-stl/stlport/stlport/string.h:
+
+/Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/../include/string.h:
+
+/Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/malloc.h:
+
+/Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/alloca.h:
+
+/Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/strings.h:
+
+/Users/Michiel/android-ndk-r9d/platforms/android-9/arch-x86/usr/include/memory.h:
+
+/Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/EventEmitter.h:
+
+/Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/JNIUtil.h:
+
+/Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/AndroidUtil.h:
+
+/Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/JSException.h:
+
+/Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/ProxyFactory.h:
+
+/Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/TypeConverter.h:
+
+/Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/V8Util.h:
+
+/Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/org.appcelerator.kroll.KrollModule.h:
+
+/Users/Michiel/Library/Application\ Support/Titanium/mobilesdk/osx/3.2.3.GA/android/native/include/Proxy.h:
